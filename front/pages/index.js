f74bdfce1e6d8b2d1227882c0c859bd5ac71f763
Martin Ippolito <84152110+martinippo01@users.noreply.github.com>
2024-04-15 01:51:30 -0300

    Update index.js

--- front/pages/index.js
+++ front/pages/index.js
@@ -25,8 +25,8 @@ export default function Home() {
                 <NavBar></NavBar>
                 <Container className='d-flex align-items-center container-landing add-space'>
                     <div className="col-lg-6 order-2 order-lg-1 d-flex flex-column justify-content-center text-center text-lg-start">
-                        <h1>Introducing Kanbana</h1>
-                        <p>Our platform uses AI to automate task management for team projects, assigning tasks based on individual skills and knowledge. Boost productivity and efficiency with intelligent task allocation.</p>
+                        <h1 className="text-primary">Introducing Kanbana</h1>
+                        <p className="text-dark">Our platform uses AI to automate task management for team projects, assigning tasks based on individual skills and knowledge. Boost productivity and efficiency with intelligent task allocation.</p>
                         <Button variant="outline-primary" style={{width: "300px"}} href="/createProject">Create your first project</Button>
                     </div>
                     <div className="col-lg-6 order-1 order-lg-2">
